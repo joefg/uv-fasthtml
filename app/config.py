7f9211812612c44382a22d6285afd33012f7ac7d
joefg <joefullergray@gmail.com>
2025-09-29 16:10:14 +0100

    tech(app)!: Use port 5001 only for dev and prod
    On dev, there is no reason to not use port 5001, and
    on prod, you should use a reverse proxy to map 5001 to 80
    anyway.
    
    This allows us to keep port numbers absolutely deterministic
    so that `./run stop` continues to function.

--- app/config.py
+++ app/config.py
@@ -7,7 +7,6 @@ load_dotenv()
 # Application settings
 APP_NAME = os.getenv("APP_NAME", "uv-fasthtml")
 FOOTER_TEXT = os.getenv("FOOTER_TEXT", "© uv-fasthtml, 2025 - all rights reserved")
-PORT = os.getenv("PORT", 5001)
 
 # Debug settings
 DEBUG = os.getenv("DEBUG", 'false').lower() == "true"
