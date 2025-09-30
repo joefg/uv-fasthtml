e2656baeff70865f53b6a5b46aefed5d03e547b5
joefg <joefullergray@gmail.com>
2025-09-30 23:51:58 +0100

    lint(app/alert): Clean up the telegram alert function

--- app/alert.py
+++ app/alert.py
@@ -4,17 +4,13 @@ import logging
 
 import config
 
+
 def telegram(msg: str) -> None:
     bot_token = config.TG_TOKEN
     chat_id = config.TG_ALERT_CHAT
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 
     if bot_token and chat_id:
-        url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-        httpx.post(
-            url, params={
-                'chat_id': chat_id,
-                'text': msg
-            }
-        )
+        httpx.post(url, params={"chat_id": chat_id, "text": msg})
     else:
         logging.warning("Telegram alert not sent. Consider setting it up.")
