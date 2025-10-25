3d5e0927906052c84fc590af211afa38408f1eb6
joefg <joefullergray@gmail.com>
2025-10-25 22:18:05 +0100

    test(integration/protected): Add test

---
+++ tests/integration/test_protected.py
@@ -0,0 +1,17 @@
+from unittest import mock
+
+from starlette.testclient import TestClient
+
+from app.main import app
+
+class TestProtected:
+    def test_unauthenticated_protected_returns_404(self):
+        client = TestClient(app)
+        request = client.get("/protected/.gitkeep")
+        assert request.status_code == 404
+
+    def test_authenticated_protected_returns_200(self):
+        cookies = {"session_name" : "session_value"}
+        client = TestClient(app, cookies=cookies)
+        request = client.get("/protected/.gitkeep")
+        assert request.status_code == 200
