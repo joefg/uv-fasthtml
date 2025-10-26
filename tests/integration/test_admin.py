8d1b4a7fe823f3f7b6f030ee8ae728e81161c1ac
joefg <joefullergray@gmail.com>
2025-10-26 18:50:23 +0000

    test(integration/admin): Add test for Admin
    This commit includes basic tests for the admin routes.
    
    Further work: Adding tests for adding and removing admin and
    activations, which requires another OAuth user (which the
    OAuth test endpoint doesn't support).

---
+++ tests/integration/test_admin.py
@@ -0,0 +1,41 @@
+import pytest
+from starlette.testclient import TestClient
+
+from app.main import app
+
+from utils import mock_auth, set_test_user_admin
+
+@pytest.fixture(autouse=True)
+def setup():
+    set_test_user_admin(True)
+
+def test_unauthenticated_users_cant_see_admin():
+    client = TestClient(app)
+    request = client.get("/admin")
+    assert request.status_code == 404
+
+def test_authenticated_users_no_admin_returns_401():
+    client = TestClient(app)
+    authenticated_client = mock_auth(client)
+    set_test_user_admin(False)
+    request = authenticated_client.get("/admin")
+    assert request.status_code == 401
+
+def test_authenticated_admins_returns_ok():
+    client = TestClient(app)
+    authenticated_client = mock_auth(client)
+    request = authenticated_client.get("/admin")
+    assert request.status_code == 200
+
+def test_authenticated_admins_can_see_user():
+    client = TestClient(app)
+    authenticated_client = mock_auth(client)
+    request = authenticated_client.get("/admin/user/459")
+    assert request.status_code == 200
+
+def test_authenticated_non_admins_cant_see_user():
+    client = TestClient(app)
+    authenticated_client = mock_auth(client)
+    set_test_user_admin(False)
+    request = authenticated_client.get("/admin/user/459")
+    assert request.status_code == 401
