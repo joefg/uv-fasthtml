82ca2af4ff250b559c23787092f11777d27b8460
joefg <joefullergray@gmail.com>
2025-11-08 21:04:05 +0000

    feature(auth): Send an alert to the dev channel for new users
    This is a demonstration of the BackgroundTasks feature provided
    by Starlette. On first auth, a Telegram alert is sent to the
    dev channel containing the GitHub username of the new user.

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,9 +1,12 @@
 from fasthtml.common import FastHTML, RedirectResponse
 from fasthtml.oauth import redir_url
 
+from starlette.background import BackgroundTasks
+
 import auth.utils
 import auth.github as github
 
+from alert import telegram
 import config
 from components import page_content as page
 from exceptions import handlers as exception_handlers
@@ -16,6 +19,15 @@ from pages.login import login as login_page
 from pages.login import account_deactivated as account_deactivated_page
 
 auth_app = FastHTML(exception_handlers=exception_handlers)
+
+def alert_new_signup(username: str):
+    alert_message = f"""
+✨ A new user has registered! ✨
+
+{username} has signed up!
+"""
+    telegram(msg=alert_message)
+
 
 @auth_app.get("/login")
 async def login(request, session):
@@ -39,7 +51,11 @@ async def oauth_redirect(code: str, request, session):
     user = get_user_by_id(user_id)
     if not user:
         register_user(user_info)
+        user = get_user_by_id(user_id)
+        tasks = BackgroundTasks()
+        tasks.add_task(alert_new_signup, username=user.gh_login)
         auth.utils.login_user(session, user_id)
+        return RedirectResponse("/", status_code=303, background=tasks)
     else:
         if not bool(user.is_active):
             return page(
