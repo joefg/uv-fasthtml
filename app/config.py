cd1d838c2efee4d089c4d07e517e361a426d9f81
joefg <joefullergray@gmail.com>
2026-03-01 16:44:47 +0000

    refactor(app/config): Remove unused environment variables
    I don't use Discord or Slack, so removing these.

--- app/config.py
+++ app/config.py
@@ -20,13 +20,6 @@ DB_PATH = os.getenv("DB_PATH", "database/database.sqlite3")
 TG_TOKEN = os.getenv("TG_TOKEN")
 TG_ALERT_CHAT = os.getenv("TG_ALERT_CHAT")
 
-# Discord alert settings
-DISCORD_WEBHOOK_ID = os.getenv("DISCORD_WEBHOOK_ID")
-DISCORD_WEBHOOK_TOKEN = os.getenv("DISCORD_WEBHOOK_TOKEN")
-
-# Slack alert settings
-SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
-
 # GitHub OAuth
 GH_OAUTH_ID = os.getenv("GH_OAUTH_ID")
 GH_OAUTH_SECRET = os.getenv("GH_OAUTH_SECRET")
