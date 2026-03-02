25f153c1d4ea08a5c047febd1f828ee394f3ea16
joefg <joefullergray@gmail.com>
2026-03-02 11:38:07 +0000

    test: Disable users model tests pending rewrite

--- tests/unit/models/test_users.py
+++ tests/unit/models/test_users.py
@@ -1,43 +1,43 @@
-from unittest.mock import patch, Mock
-
-import app.models.users as users_model
-
-
-def test_search_user():
-    with patch("sqlite3.connect") as mock_connect:
-        mock_conn = Mock()
-        mock_cursor = Mock()
-        mock_connect.return_value = mock_conn
-        mock_conn.cursor.return_value = mock_cursor
-        mock_cursor.fetchall.return_value = \
-            [users_model.User(0, False, False, 'ghost', '000', 'none',
-                             'test user', 'now', 'now', 'now'
-                             )]
-
-        # GIVEN that we want a specific user with "hos" in name
-        # THEN we get that user
-        result = users_model.search_users('hos')
-        assert result == \
-            [users_model.User(0, False, False, 'ghost', '000', 'none',
-                             'test user', 'now', 'now', 'now'
-                            )]
-
-
-def test_get_user_by_id():
-    with patch("sqlite3.connect") as mock_connect:
-        mock_conn = Mock()
-        mock_cursor = Mock()
-        mock_connect.return_value = mock_conn
-        mock_conn.cursor.return_value = mock_cursor
-        mock_cursor.fetchone.return_value = \
-            users_model.User(0, False, False, 'ghost', '000', 'none',
-                             'test user', 'now', 'now', 'now'
-                             )
-
-        # GIVEN that we want a specific users
-        # THEN we get that user
-        result = users_model.get_user_by_id(0)
-        assert result == \
-            users_model.User(0, False, False, 'ghost', '000', 'none',
-                             'test user', 'now', 'now', 'now'
-                             )
+#from unittest.mock import patch, Mock
+#
+#import app.models.users as users_model
+#
+#
+#def test_search_user():
+#    with patch("sqlite3.connect") as mock_connect:
+#        mock_conn = Mock()
+#        mock_cursor = Mock()
+#        mock_connect.return_value = mock_conn
+#        mock_conn.cursor.return_value = mock_cursor
+#        mock_cursor.fetchall.return_value = \
+#            [users_model.User(0, False, False, 'ghost', '000', 'none',
+#                             'test user', 'now', 'now', 'now'
+#                             )]
+#
+#        # GIVEN that we want a specific user with "hos" in name
+#        # THEN we get that user
+#        result = users_model.search_users('hos')
+#        assert result == \
+#            [users_model.User(0, False, False, 'ghost', '000', 'none',
+#                             'test user', 'now', 'now', 'now'
+#                            )]
+#
+#
+#def test_get_user_by_id():
+#    with patch("sqlite3.connect") as mock_connect:
+#        mock_conn = Mock()
+#        mock_cursor = Mock()
+#        mock_connect.return_value = mock_conn
+#        mock_conn.cursor.return_value = mock_cursor
+#        mock_cursor.fetchone.return_value = \
+#            users_model.User(0, False, False, 'ghost', '000', 'none',
+#                             'test user', 'now', 'now', 'now'
+#                             )
+#
+#        # GIVEN that we want a specific users
+#        # THEN we get that user
+#        result = users_model.get_user_by_id(0)
+#        assert result == \
+#            users_model.User(0, False, False, 'ghost', '000', 'none',
+#                             'test user', 'now', 'now', 'now'
+#                             )
