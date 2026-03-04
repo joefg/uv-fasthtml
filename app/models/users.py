e4d3bc7de4a00f81510820d110325ac8439993d3
joefg <joefullergray@gmail.com>
2026-03-04 13:27:48 +0000

    fix: use correct parameter user_id in set_user_admin and set_user_active

--- app/models/users.py
+++ app/models/users.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
-from sqlmodel import (
-    select
-)
+from sqlmodel import select
 
 from .models import User
 from database import connect
@@ -27,16 +25,16 @@ def get_user_by_id(id: int) -> User:
 
 
 def register_user(gh_user_info: dict) -> None:
-    created_at = datetime.fromisoformat(gh_user_info['created_at'])
+    created_at = datetime.fromisoformat(gh_user_info["created_at"])
     new_user = User(
-        id=gh_user_info['id'],
+        id=gh_user_info["id"],
         is_active=True,
         is_admin=False,
-        gh_login=gh_user_info['login'],
-        gh_node_id=gh_user_info['node_id'],
-        gh_avatar_url=gh_user_info['avatar_url'],
-        gh_type=gh_user_info['type'],
-        gh_created_at=created_at
+        gh_login=gh_user_info["login"],
+        gh_node_id=gh_user_info["node_id"],
+        gh_avatar_url=gh_user_info["avatar_url"],
+        gh_type=gh_user_info["type"],
+        gh_created_at=created_at,
     )
     with connect() as session:
         session.add(new_user)
@@ -44,15 +42,15 @@ def register_user(gh_user_info: dict) -> None:
 
 
 def update_user(gh_user_info: dict) -> None:
-    id = gh_user_info['id']
+    id = gh_user_info["id"]
     with connect() as session:
         statement = select(User).where(User.id == id)
         updated_user = session.exec(statement).one()
         updated_user.is_active = True
-        updated_user.gh_login = gh_user_info['login']
-        updated_user.gh_node_id = gh_user_info['node_id']
-        updated_user.gh_avatar_url = gh_user_info['avatar_url']
-        updated_user.gh_type = gh_user_info['type']
+        updated_user.gh_login = gh_user_info["login"]
+        updated_user.gh_node_id = gh_user_info["node_id"]
+        updated_user.gh_avatar_url = gh_user_info["avatar_url"]
+        updated_user.gh_type = gh_user_info["type"]
         session.add(updated_user)
         session.commit()
 
@@ -86,7 +84,7 @@ def update_last_login(user_id: int) -> None:
 
 def set_user_admin(user_id: int, change_to: bool) -> None:
     with connect() as session:
-        statement = select(User).where(User.id == id)
+        statement = select(User).where(User.id == user_id)
         user = session.exec(statement).one()
         user.is_admin = change_to
         session.add(user)
@@ -95,7 +93,7 @@ def set_user_admin(user_id: int, change_to: bool) -> None:
 
 def set_user_active(user_id: int, change_to: bool) -> None:
     with connect() as session:
-        statement = select(User).where(User.id == id)
+        statement = select(User).where(User.id == user_id)
         user = session.exec(statement).one()
         user.is_active = change_to
         session.add(user)
